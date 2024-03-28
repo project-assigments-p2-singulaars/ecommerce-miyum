5c759566d019d40a830759e601737870998aa714
Daniel Cueto <dcueto14.info@gmail.com>
2024-03-28 13:56:19 +0100

    feat: burger-menu-view done on js

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,4 +1,73 @@
 
 import { burgerMenuView } from './burgerMenuView.js';
+import { productsComponent } from './productsComponent.js';
 
-burgerMenuView();
+document.addEventListener('DOMContentLoaded', initApp);
+
+function initApp(){
+
+
+  const productsData = [
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+    {
+      name: 'Chocho Yum',
+      description: 'Chocho is our brand cake, made with love <3',
+      price: '5',
+      type: 'Chocolate',
+      category: 'Cake Slices',
+      image: './static/images/image_cake1.png',
+    },
+
+  ]
+
+  burgerMenuView();
+  productsComponent( productsData );
+
+}
