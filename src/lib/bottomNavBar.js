8571d6d31363b8f741e67ce44c47c3942010b0ea
Daniel Cueto <dcueto14.info@gmail.com>
2024-04-02 12:03:22 +0200

    fix: border styles changed; disapears on desktop(media query)

--- src/lib/bottomNavBar.js
+++ src/lib/bottomNavBar.js
@@ -1,15 +1,15 @@
 
-export function navbar(){
-    const body = document.querySelector("body");
-    const navBarContainer = document.createElement("div");
-    navBarContainer.classList.add("bottom-nav-bar");
-    
-    navBarContainer.innerHTML = `
-        <button class="fa-solid fa-house" style="color: #000000;"></button>
-        <button class="fa-solid fa-user" style="color: #000000;"></button>
-        <button class="fa-solid fa-bag-shopping" style="color: #000000;"></button>
-        <button class="fa-solid fa-bars" style="color: #000000;"></button>
-    `;
-    
-    body.appendChild(navBarContainer);
+export function navbar() {
+  const body = document.querySelector("body");
+  const navBarContainer = document.createElement("div");
+  navBarContainer.classList.add("bottom-nav-bar");
+
+  navBarContainer.innerHTML = /* html */`
+    <button class="fa-solid fa-house""></button>
+    <button class="fa-solid fa-user""></button>
+    <button class="fa-solid fa-bag-shopping""></button>
+    <button class="fa-solid fa-bars""></button>
+  `;
+
+  body.appendChild(navBarContainer);
 }
