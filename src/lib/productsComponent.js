8f31d71947db0088d2e0060b9a7e3a6315be3b62
Daniel Cueto <dcueto14.info@gmail.com>
2024-04-08 15:17:18 +0200

    feature: change products by category

--- src/lib/productsComponent.js
+++ src/lib/productsComponent.js
@@ -1,24 +1,13 @@
+import { categorySelector } from "./categorySelector.js";
 
 export function productsComponent( array ){
+
+  /* MAQUETACIÓN HTML */
 
   const main = document.querySelector('main');
 
   const productsComponent = document.createElement('section');
   productsComponent.classList.add('products-component');
-
-  const categoryNavBar = document.createElement('nav');
-  categoryNavBar.classList.add('categories-nav', 'padding-wrap');
-  categoryNavBar.innerHTML = /* html */`
-    <ul class="categories-nav__categories-tabs">
-      <li class="categories-tabs__tab-item" id="tab-category-pastries"><i class="tab-item__icon icon-croissant"></i><span class="tab-item__text">Pastries</span></li>
-      <li class="categories-tabs__tab-item" id="tab-category-cookies"><i class="tab-item__icon icon-cookie"></i><span class="tab-item__text">Cookies</span></li>
-      <li class="categories-tabs__tab-item active" id="tab-category-cakes"><i class="tab-item__icon icon-cake" ></i><span class="tab-item__text">Cakes</span></li>
-      <li class="categories-tabs__tab-item" id="tab-category-donuts"><i class="tab-item__icon icon-donut"></i><span class="tab-item__text">Donuts</span></li>
-      <li class="categories-tabs__tab-item" id="tab-category-breads"><i class="tab-item__icon icon-bread"></i><span class="tab-item__text">Breads</span></li>
-    </ul>
-  `;
-
-  document.body.appendChild(categoryNavBar);
 
   const productsSection = document.createElement('section');
   productsSection.classList.add('products');
@@ -35,44 +24,47 @@ export function productsComponent( array ){
 
   productsSection.appendChild(categoryTitle);
 
-let fetchData;
+
+  let fetchData;
+
+  fetch ('http://localhost:3000/products')
+    .then(response => response.json())
+    .then(data => {
+      fetchData = data;
+    })
+    .finally( () => {
+      renderProducts( fetchData, productsContainer );
+
+      productsSection.appendChild(productsContainer);
+      productsComponent.appendChild( productsSection );
+      main.appendChild(productsComponent);
+    });
+
+  categorySelector( productsComponent );
+}
 
 
-fetch ('http://localhost:3000/products')
-  .then(response => response.json())
-  .then(data => {
-    fetchData = data;
 
-    fetchData.forEach( ( product ) => {
-      const productEl = renderProductElement( product );
-      productsContainer.appendChild( productEl );
-    } );
-      
-    productsSection.appendChild(productsContainer);
-    productsComponent.appendChild( categoryNavBar );
-    productsComponent.appendChild( productsSection );
-      
-    main.appendChild(productsComponent);
+export function renderProducts( data, productsContainer ){
+  data.forEach( ( product ) => {
+    const productEl = renderProductElement( product );
+    productsContainer.appendChild( productEl );
+  });
+}
 
-    renderProductData();
-  
-  function renderProductElement( product ){
 
+export function renderProductElement( product ) {
   const productEl = document.createElement('article');
-  productEl.classList.add('products__product-card');
-
+  productEl.classList.add('products__product-card')
   productEl.innerHTML = /* html */`
     <a href="./pages/product-detail-view.html">
-      <span class="product-card__price-tag">${ product.price }€</span><button class="product-card__add-to-cart-btn"><i>+</i></button>
-      <figure class="product-card__image"><img src=${ product.image_url } alt="${ product.name }"></figure>
+      <span class="product-card__price-tag">${product.price}€</span><button class="product-card__add-to-cart-btn"><i>+</i></button>
+      <figure class="product-card__image"><img src=${product.image_url} alt="${product.name}"></figure>
       <div class="product-card__info">
-        <h4 class="product-card__name">${ product.name }</h4>
-        <span class="product-card__category">${ product.flavor }</span>
+        <h4 class="product-card__name">${product.name}</h4>
+        <span class="product-card__category">${product.flavor}</span>
       </div>
     </a>
   `;
-  
-    return productEl;
-    }
-  }
-  )}
+  return productEl;
+}
