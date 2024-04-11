1c3a2673344149e1e61bcdb27948e3c1900e886c
Daniel Cueto <dcueto14.info@gmail.com>
2024-04-11 11:45:01 +0200

    feat: our favorites component functionality

--- src/lib/favoritesProducts.js
+++ src/lib/favoritesProducts.js
@@ -1,5 +1,4 @@
-
-export function favoritesProducts( ){
+export async function favoritesProducts( ){
 
   const mainEl = document.querySelector('main');
 
@@ -15,75 +14,63 @@ export function favoritesProducts( ){
 
   favoritesSection.appendChild(titleSection);
 
-
-  favoritesContainer.innerHTML = /* html */`
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-    <article class="favorites-products__item">
-      <img src="/src/static/images/image_cake1.png" alt="product" class="item__image" />
-      <div class="item__details">
-        <p class="details__title">Chocho Yum</p>
-        <span class="details__flavor">Chocolate</span>
-      </div>
-    </article>
-  `;
+  const ourFavoriteProducts = await getOurFavoriteProducts();
+  renderOurFavoritesProducts( ourFavoriteProducts, favoritesContainer );
 
 
   favoritesSection.appendChild(favoritesContainer);
-
-
-
-
-
-  
-
   document.body.insertBefore(favoritesSection, mainEl);
+
+}
+
+
+async function getOurFavoriteProducts(){
+
+  const endpoint = 'http://localhost:3000/products?our_favorite=true';
+  const request = await fetch( endpoint );
+  const response = await request.json();
+
+  return response;
+
+}
+
+
+function renderOurFavoritesProducts( data, productsContainer ){
+  data.forEach( ( product ) => {
+    const productEl = renderFavoriteProduct( product );
+    productsContainer.appendChild( productEl );
+  });
+}
+
+
+function renderFavoriteProduct( product ){
+
+  const productEl = document.createElement('article');
+  productEl.dataset.id = product.id;
+  productEl.classList.add('favorites-products__item');
+
+  productEl.innerHTML = /* html */`
+    <img src=${product.image_url} alt=${product.name} class="item__image" data-id=${product.id} />
+    <div class="item__details" data-id=${product.id}>
+      <p class="details__title" data-id=${product.id}>${product.name}</p>
+      <span class="details__flavor" data-id=${product.id}>${product.flavor}</span>
+    </div>
+  `;
+
+
+  productEl.addEventListener('click', e => {
+
+    const productDetailViewRoute = './pages/product-detail-view.html';
+
+    if( e.target.dataset.id ){
+      localStorage.setItem('product-selected', e.target.dataset.id);
+    }
+
+    window.location.href = productDetailViewRoute;
+
+  });
+
+
+  return productEl;
 
 }
